e80b3a0789721a154b26d708e2d62842bad9e8e5
Habiba A. Elbehairy <101597165+habibaelbehairy@users.noreply.github.com>
2025-07-10 14:00:09 +0300

    Refactor code style in configuration and test runner files for consistency

--- UI/nightwatch.conf.js
+++ UI/nightwatch.conf.js
@@ -1,53 +1,53 @@
-const chromedriver = require('chromedriver');
-const geckodriver = require('geckodriver');
+const chromedriver = require("chromedriver");
+const geckodriver = require("geckodriver");
 
 module.exports = {
   src_folders: ["./tests"],
   page_objects_path: ["./page-objects"],
-  
+
   webdriver: {
     start_process: true,
     port: 9515,
-    server_path: chromedriver.path
+    server_path: chromedriver.path,
   },
   // this is commented to generate html report as it gives error when running tests after a failure
   // Uncomment to run tests after a failure
 
-  // skip_testcases_on_fail: false, 
+  // skip_testcases_on_fail: false,
   test_settings: {
     default: {
       desiredCapabilities: {
         browserName: "chrome",
         chromeOptions: {
-          args: ["--no-sandbox", "--disable-dev-shm-usage"]
-        }
-      }
+          args: ["--no-sandbox", "--disable-dev-shm-usage"],
+        },
+      },
     },
     chrome: {
       webdriver: {
         server_path: chromedriver.path,
         start_process: true,
-        port: 9515
+        port: 9515,
       },
       desiredCapabilities: {
         browserName: "chrome",
         chromeOptions: {
-          args: ["--no-sandbox", "--disable-dev-shm-usage"]
+          args: ["--no-sandbox", "--disable-dev-shm-usage"],
         },
-        "goog:loggingPrefs": { browser: 'ALL' }
-      }
+        "goog:loggingPrefs": { browser: "ALL" },
+      },
     },
     firefox: {
       webdriver: {
         server_path: geckodriver.path,
-        port: 4444
+        port: 4444,
       },
       desiredCapabilities: {
         browserName: "firefox",
         "moz:firefoxOptions": {
-          args: ["--headless"]
-        }
-      }
-    }
-  }
+          args: ["--headless"],
+        },
+      },
+    },
+  },
 };
